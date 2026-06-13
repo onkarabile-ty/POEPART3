8eade03b3bab2af48f30e45b0ef7938c97de19bc
Student <Student@LabVM2047644.pmlkh15zwvve1bp0hs3vh5gbya.jnbx.internal.cloudapp.net>
2026-06-13 19:44:46 +0200

    commit3

--- src/test/java/MessagePart3Test.java
+++ src/test/java/MessagePart3Test.java
@@ -19,66 +19,6 @@ public class MessagePart3Test {
                 "Did you get the cake?",
                 Message.sentMessages.get(0).getMessage()
         );
-    }
+    
 
-    @Test
-    public void testLongestMessage() {
-
-        Message msg =
-                new Message(
-                        2,
-                        "+27838884567",
-                        "Where are you? You are late! I have asked you to be on time."
-                );
-
-        Message.storedMessages.add(msg);
-
-        assertEquals(
-                "Where are you? You are late! I have asked you to be on time.",
-                Message.storedMessages.get(0).getMessage()
-        );
-    }
-
-    @Test
-    public void testRecipientSearch() {
-
-        Message msg =
-                new Message(
-                        5,
-                        "+27838884567",
-                        "Ok, I am leaving without you."
-                );
-
-        Message.storedMessages.add(msg);
-
-        assertEquals(
-                "+27838884567",
-                msg.getRecipient()
-        );
-    }
-
-    @Test
-    public void testDeleteMessage() {
-
-        Message msg =
-                new Message(
-                        2,
-                        "+27838884567",
-                        "Where are you?"
-                );
-
-        Message.storedMessages.add(msg);
-
-        int before =
-                Message.storedMessages.size();
-
-        Message.deleteMessageByHash(
-                msg.getMessageHash()
-        );
-
-        int after =
-                Message.storedMessages.size();
-
-        assertEquals(before - 1, after);
-    }
-}
+   
